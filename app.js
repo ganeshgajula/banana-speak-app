aa19db95a1156e86feeabebdcfc9deef81a87140
ganesh <ganeshgajula22@gmail.com>
2020-11-30 12:18:19 +0530

    feat: ex:03 - wiring button click

--- app.js
+++ app.js
@@ -2,5 +2,15 @@
 // alert("this is working!!");
 
 // Prompt
-let userName = prompt("Enter your name");
-alert("Welcome to my project " + userName);
+// let userName = prompt("Enter your name");
+// alert("Welcome to my project " + userName);
+
+let btnTranslate = document.querySelector("#btn-translate");
+btnTranslate.addEventListener("click", function () {
+  console.log("clicked!");
+});
+
+btnTranslate.addEventListener("click", clickHandler);
+function clickHandler() {
+  console.log("clicked!!");
+}
